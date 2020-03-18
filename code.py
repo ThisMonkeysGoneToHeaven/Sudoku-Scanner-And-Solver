3a07973a76ba43001b0b3d900cc377b6de0ecc4d
Anant Agarwal <32913413+DiAnant@users.noreply.github.com>
2020-03-19 05:15:21 +0530

    everything works

--- code.py
+++ code.py
@@ -2,20 +2,57 @@
 import sys
 import numpy
 import cv2 as cv
-from PIL import Image, ImageOps, ImageEnhance
+from tensorflow.keras import models
+from PIL import Image, ImageOps, ImageEnhance, ImageDraw
+
+def recognize(model, test):
+	arr = numpy.array(test)
+	arr = arr/255
+	arr = arr.reshape(-1,28,28,1)
+	num = numpy.argmax(model.predict(arr))
+	return num
 
 def whiteness(pic):
-	total_pixels = pic.size[0]*pic.size[1]
+	size = pic.size[0]
+	d= 5 ## defining a boundary
+	pic = pic.crop((d, d, size-d, size-d))
+	total_pixels = size*size
 	# white_pixels = numpy.sum(numpy.array(pic)==255)
 	white_pixels = numpy.sum(numpy.array(pic) > 185)
 	return (white_pixels/total_pixels)
 
-# loading the image
+## Removing grid lines
+def draw_line(x1, y1, x2, y2, w):
+    shape = [(x1, y1), (x2, y2)]
+    img1 = ImageDraw.Draw(image)
+    img1.line(shape, fill="white", width=w)
+
+def lines():
+    line_distance = int(size[1]/9)
+    dis_covered = 0
+    line_no = 10
+    thick = 5
+    for i in range(line_no):
+        if i == 3 or i == 6:
+            thick = 15
+        else:
+            thick = 15
+            # Vertical
+        draw_line(dis_covered, 0, dis_covered, size[1], thick)
+        # Horizontal
+        draw_line(0, dis_covered, size[0], dis_covered, thick)
+        dis_covered += line_distance
+
+
+## loading the image
 image = Image.open('image.jpg').convert('RGB')
 
 # resizing image upto a Good size
 size = (400,400)
 image = image.resize(size)
+
+# calling the lines() function
+lines()
 
 # adding brightness to image
 enhancer = ImageEnhance.Brightness(image)
@@ -89,14 +126,32 @@ for i in range(80):
 		rindex = i+1
 mid = whites[rindex]-0.05
 
+## making a board for containing potential numbers
+board = [[None for _ in range(9)] for _ in range(9)]
+
+## count is a variable made to count the number
+## of squares with inputs calculated by this algorithm
 count = 0
 for i in range(9):
 	for j in range(9):
 		whites = whiteness(grid[i][j])
-		# print(whites)
-		# grid[i][j].show()
 		if whites > mid:
 			count += 1
-print(count)
-image.show()
+		else:
+			board[i][j] = 0;
+# print(count)
 
+## loading the tensorflow model stored locally
+model = models.load_model('weights.h5')
+
+## storing all the results in board array
+for i in range(9):
+	for j in range(9):
+		if board[i][j] == None:
+			board[i][j] = recognize(model, grid[i][j])
+
+## printing the damn board
+# for i in range(9):
+# 	for j in range(9):
+# 		print(board[i][j], end = ' ')
+# 	print('\n')
--- recognize.py
+++
@@ -1,5 +0,0 @@
-import numpy as np 
-import tensorflow as tf
-import matplotlib.pyplot as plt
-
-
